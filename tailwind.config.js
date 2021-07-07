83d637528cd34c926f089195683cc5d5a7b16fef
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2021-07-07 17:56:11 -0400

    set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
