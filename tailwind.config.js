105ad7fa8152f14bcebf14b1a473d458c2a7edb8
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2021-07-13 16:56:03 -0400

    config fontSize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,18 @@ module.exports = {
       DEFAULT: theme('colors.bordercolor'),
     }),
 
+    fontSize: {
+      hero: 'clamp(2.5rem, 2.1rem + 2vw, 4.5rem)', // 40~72px
+      h1: 'clamp(1.75rem, 1.5rem + 1.25vw, 3rem)', // 28~48px
+      h2: 'clamp(1.5rem, 1.4rem + 0.5vw, 2rem)', // 24~32px,
+      xl: '1.5rem',
+      lg: '1.125rem',
+      h3: 'clamp(1rem, 1rem + 0.2vw, 1.25rem)', // 16~20px
+      body: '1rem',
+      sm: '0.875rem',
+      xs: '0.75rem',
+    },
+
     extend: {
       spacing: {
         xs: '0.25rem',
