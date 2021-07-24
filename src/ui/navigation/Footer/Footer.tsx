9285e39f19428a7e3f8dbd92aeb1cd2d137e487d
Andrew <phongnguyentuan20@gmail.com>
2021-07-24 01:28:24 -0400

    styled footer

--- src/ui/navigation/Footer/Footer.tsx
+++ src/ui/navigation/Footer/Footer.tsx
@@ -1,33 +1,35 @@
-import { ButtonLink, ButtonPrimary } from '@ui/Button/Button';
+import { ButtonLink } from '@ui/Button/Button';
 
 type Props = {
   className?: string;
 };
 function Footer({ className }: Props) {
   return (
-    <footer className={className} tw="mb-3xl px-sm md:px-md">
-      <div tw="border-t mx-auto pt-2xl pb-3xl">
+    <footer className={className} tw="border-t px-sm md:px-md">
+      <div tw="mx-auto pt-2xl pb-3xl flex flex-col gap-2xl md:(flex-row gap-4xl)">
+        <div>
+          <h3 tw="font-bold">Andrew Nguyen</h3>
+          <ButtonLink
+            href="mailto:hey@andrewnt.dev"
+            target="_blank"
+            rel="noopener"
+          >
+            hey@andrewnt.dev
+          </ButtonLink>
+        </div>
+
         <nav aria-label="Links to my other social media">
-          <ul tw="grid grid-cols-2 md:grid-cols-3">
+          <h3 tw="font-bold">Find me on the internet</h3>
+          <ul tw="flex gap-md flex-wrap">
             {mediaLinks.map((link) => (
               <li key={link.href}>
-                <ButtonLink href={link.href} target="_blank" rel="noreferrer">
+                <ButtonLink href={link.href} target="_blank" rel="noopener">
                   {link.label}
                 </ButtonLink>
               </li>
             ))}
           </ul>
         </nav>
-        <ButtonPrimary
-          tw="text-center"
-          as="a"
-          href="https://www.buymeacoffee.com/andrewnt219"
-          target="_blank"
-          rel="noopener"
-          size="sm"
-        >
-          Buy me a coffee
-        </ButtonPrimary>
       </div>
     </footer>
   );
@@ -47,16 +49,20 @@ const mediaLinks: Link[] = [
     label: 'Github',
   },
   {
-    href: 'https://www.facebook.com/phong.nguyentuan.108',
-    label: 'Facebook',
-  },
-  {
     href: 'https://www.linkedin.com/in/andrewnt219/',
     label: 'LinkedIn',
   },
   {
     href: 'https://twitter.com/andrewnt219',
     label: 'Twiiter',
+  },
+  {
+    href: 'https://www.facebook.com/phong.nguyentuan.108',
+    label: 'Facebook',
+  },
+  {
+    href: 'https://www.buymeacoffee.com/andrewnt219',
+    label: 'Buy me a coffee',
   },
 ];
 export default Footer;
